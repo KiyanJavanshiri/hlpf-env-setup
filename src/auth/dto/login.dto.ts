49b400f6028d2080fb8fe6a78a9a334e5e4ed1e2
kiyan javanshiri <kiyanjavanshiri@gmail.com>
2026-05-04 17:18:11 +0300

    add swagger with ApiProperty decorators on DTOs

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,9 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString } from 'class-validator';
 
 export class LoginDto {
+  @ApiProperty({
+    example: 'example@gmail.com',
+    description: 'Елетронна пошта користувача',
+  })
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    example: 'hgtj_12_pm',
+    description: 'Пароль користувача',
+  })
   @IsString()
   password: string;
 }
--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import {
   IsEmail,
   IsString,
@@ -7,14 +8,29 @@ import {
 } from 'class-validator';
 
 export class RegisterDto {
+  @ApiProperty({
+    example: 'example@gmail.com',
+    description: 'Елетронна пошта користувача',
+  })
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    example: 'hgtj_12_pm',
+    description: 'Пароль користувача',
+    minLength: 8,
+    maxLength: 128,
+  })
   @IsString()
   @MinLength(8)
   @MaxLength(128)
   password: string;
 
+  @ApiPropertyOptional({
+    example: 'John Doe',
+    description: "Ім'я користувача",
+    maxLength: 100,
+  })
   @IsOptional()
   @IsString()
   @MaxLength(100)
